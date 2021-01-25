fca67cfe028918fbeac7e603298b3331356c65af
Stephen Gold <sgold@sonic.net>
2021-01-25 13:06:51 -0800

    bump version to 0.1.0 and create release-log.md

---
+++ GarrettLibrary/release-log.md
@@ -0,0 +1,6 @@
+# release log for the Garrett Library
+
+## Version 0.1.0 released on TBD
+
++ Added OrbitCamera and its supporting classes, based on code
+  from the BatsV2 project.
--- GarrettLibrary/src/main/java/com/github/stephengold/garrett/GarrettVersion.java
+++ GarrettLibrary/src/main/java/com/github/stephengold/garrett/GarrettVersion.java
@@ -59,6 +59,6 @@ public class GarrettVersion {
      * @return the branch and revision string (not null, not empty)
      */
     public static String versionShort() {
-        return "master 0.1.0-SNAPSHOT";
+        return "master 0.1.0";
     }
 }
