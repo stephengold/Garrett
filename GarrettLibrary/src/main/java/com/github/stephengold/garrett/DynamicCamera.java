7921ea379f63902cb15eecf227586a409b1eebda
Stephen Gold <sgold@sonic.net>
2022-11-17 23:04:34 -0800

    DynamicCamera: split maxAbsDot into 2 parameters

--- GarrettLibrary/src/main/java/com/github/stephengold/garrett/DynamicCamera.java
+++ GarrettLibrary/src/main/java/com/github/stephengold/garrett/DynamicCamera.java
@@ -84,10 +84,15 @@ public class DynamicCamera
     // fields
 
     /**
-     * maximum magnitude of the dot product between the camera's look direction
-     * and its preferred "up" direction
+     * maximum dot product between the camera's look direction and its preferred
+     * "up" direction (constrains looking up)
      */
-    private double maxAbsDot = Math.cos(0.3);
+    private double maxDot = Math.cos(0.3);
+    /**
+     * minimum dot product between the camera's look direction and its preferred
+     * "up" direction (constrains looking down)
+     */
+    private double minDot = -Math.cos(0.3);
     /**
      * signal-based translation speed (in psu per second, &ge;0)
      */
@@ -277,8 +282,8 @@ public class DynamicCamera
     }
 
     /**
-     * Alter the size of the pole-exclusion cone, which prevents the Camera from
-     * looking too near the preferred "up" direction or its opposite.
+     * Alter the apertures of the pole-exclusion cones, which prevent the Camera
+     * from looking too near its preferred "up" direction or its opposite.
      *
      * @param minAngle the minimum angle between the camera axis and the
      * preferred "up" direction, half the aperture of the exclusion cone (in
@@ -286,7 +291,36 @@ public class DynamicCamera
      */
     public void setPoleExclusionAngle(float minAngle) {
         Validate.inRange(minAngle, "minimum angle", 0f, FastMath.HALF_PI);
-        this.maxAbsDot = Math.cos(minAngle);
+
+        double cos = Math.cos(minAngle);
+        if (cos < 0.0) {
+            cos = 0.0;
+        }
+        this.maxDot = cos;
+        this.minDot = -cos;
+        assert maxDot > minDot : cos;
+    }
+
+    /**
+     * Alter the apertures of the pole-exclusion cones, which prevent the Camera
+     * from looking too near its preferred "up" direction or its opposite.
+     *
+     * @param upperAngle the minimum angle between the camera axis and the
+     * preferred "up" direction, half the aperture of the upper exclusion cone
+     * (in radians, &ge;0, &lt;{@code PI - minDownAngle}, default=0.3)
+     * @param lowerAngle the minimum angle between the camera axis and the
+     * preferred "down" direction, half the aperture of the lower exclusion cone
+     * (in radians, &ge;0, &lt;{@code PI - minUpAngle}, default=0.3)
+     */
+    public void setPoleExclusionAngles(float upperAngle, float lowerAngle) {
+        Validate.inRange(upperAngle, "upper angle", 0f, FastMath.PI);
+        Validate.inRange(lowerAngle, "lower angle", 0f, FastMath.PI);
+        float sum = upperAngle + lowerAngle;
+        Validate.require(sum < FastMath.PI, "sum of angles less than pi");
+
+        this.maxDot = Math.cos(upperAngle);
+        this.minDot = -Math.cos(lowerAngle);
+        assert maxDot > minDot;
     }
 
     /**
@@ -443,22 +477,25 @@ public class DynamicCamera
          */
         tmpLook.normalizeLocal();
         double dot = MyVector3f.dot(tmpLook, preferredUpDirection);
-        if (Math.abs(dot) > maxAbsDot) {
-            preferredUpDirection.mult((float) dot, tmpProj);
-            tmpLook.subtract(tmpProj, tmpRej);
-            double rejL2 = MyVector3f.lengthSquared(tmpRej);
-            if (rejL2 > 0.0) { // not directly above or below
-                double newDot = MyMath.clamp(dot, maxAbsDot);
-                double projCoefficient = newDot / dot;
-                double rejCoefficient
-                        = Math.sqrt((1.0 - newDot * newDot) / rejL2);
-                tmpProj.mult((float) projCoefficient, tmpLook);
-                MyVector3f.accumulateScaled(
-                        tmpLook, tmpRej, (float) rejCoefficient);
-            } else {
+        if (!MyMath.isBetween(minDot, dot, maxDot)) {
+            // looking in an excluded direction
+
+            // tmpRej <- the horizontal direction closest to the look direction
+            if (dot >= 1.0 || dot <= -1.0) {
+                // looking directly up or down: pick a new direction
                 MyVector3f.generateBasis(tmpLook, tmpProj, tmpRej);
-                tmpLook.set(tmpProj);
+                tmpLook.set(tmpRej);
+                dot = MyVector3f.dot(tmpLook, preferredUpDirection);
+            } else {
+                preferredUpDirection.mult((float) dot, tmpProj);
+                tmpLook.subtract(tmpProj, tmpRej);
+                MyVector3f.normalizeLocal(tmpRej);
             }
+
+            double newDot = MyMath.clamp(dot, minDot, maxDot);
+            preferredUpDirection.mult((float) newDot, tmpLook);
+            float rejCoefficient = (float) MyMath.circle(newDot);
+            MyVector3f.accumulateScaled(tmpLook, tmpRej, rejCoefficient);
         }
 
         // Apply the new "look" direction to the Camera.
